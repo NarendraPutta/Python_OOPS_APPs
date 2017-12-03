9e507c811acd6fe23d986fd201646dd8fb57086d
Narendra <naren4everr@gmail.com>
2017-12-03 18:31:29 +0530

    making changes in firstbranch

---
+++ OOPS_APPS2.1.py
@@ -0,0 +1,29 @@
+How to Install Anaconda + Python on the Windows.
+
+Download Anaconda @ https://www.anaconda.com/download/
+
+Note: 515MB file. I have already downloaded  & will give you when you need it.
+Please inform me when you need it. I will install in your PC.
+Installation is process is very simple. Just click on next buttons untill you see finish button.
+
+Once installation procedure is done. Go to Run -> Type 'cmd' -> Press 'Enter' would
+launch windows command prompt.
+
+Now type 'python' on the command prompt & press enter would launch the Python.
+Now Type 
+>>>import tkinter -> Press 'Enter'
+>>>import turtle -> Press 'Enter'
+
+If you dont see any errors. you are perfectly on the track. Incase if you see any issues,
+Please do let me know. I will try to fix the problem & help you up with the setup.
+
+We will end the class here. Thank you for choosing the OOPs_Apps Course & We will learn
+fundamentals of developing the Apps by end of the Course.
+
+
+
+
+
+
+
+
