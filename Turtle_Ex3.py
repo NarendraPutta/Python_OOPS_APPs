929f0a03a8420b59962f199d6a271225cf9bbd38
Narendra <naren4everr@gmail.com>
2017-12-03 18:22:17 +0530

    adding 3 files together

---
+++ Turtle_Ex3.py
@@ -0,0 +1,21 @@
+import turtle
+my_turtle = turtle.Turtle()
+
+def square():#Function Definition
+
+
+     #Function Parameters
+     my_turtle.forward(100)
+     my_turtle.left(90)
+     my_turtle.forward(100)
+     my_turtle.left(90)
+     my_turtle.forward(100)
+     my_turtle.left(90)
+     my_turtle.forward(100)
+    
+
+#Function Call
+square()
+
+
+
