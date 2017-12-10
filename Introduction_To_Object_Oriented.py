f8dd7ef0e5cda2b45653b23ac376e7d89930264d
Narendra <naren4everr@gmail.com>
2017-12-10 18:23:41 +0530

    OOPs Introduction

---
+++ Introduction_To_Object_Oriented.py
@@ -0,0 +1,58 @@
+# -*- coding: cp1252 -*-
+
+Learning to Speak Object Oriented:
+
+As you are aware, In all the programs we wrote till now,
+we have designed our program around passing the single, multiple line statements, control statemtns,
+functions  to view the output (i.e. All these are different blocks of statements which manipulate data and
+give us the desired output)
+
+All that we are learnt so far is called 'Procedure-oriented way of Programming'
+
+Now there is onemore beautiful way of organizing your program which is to combine data
+and functionality and wrap it inside something called an 'object' . object oriented programming is
+specially useful when large projects need to be designed. Just a theory. Dont worry even if you dont get it.
+
+'classes' and 'objects' are the two main aspects of object oriented programming.
+A class created a new type where the objects are instance of a class.
+
+An analogy is that you can have variables of type int which translates to saying that
+variables that store integers are variables which are instances (objects) of the int class.
+
+int a = 20
+int b = 35
+
+Here a & b are objects(instances) of int class. Hope this is clear. You will get it easily once explain it.
+
+In this exercise, I’m going to teach you how to speak “object oriented.”
+What I’ll do is give you a small set of words with definitions you need to know. Then I’ll give you a set of sentences with
+holes in them that you’ll have to understand. Finally, I’m going to give you a large set of exercises
+that you have to complete to make these sentences solid in your vocabulary.
+
+These are the words that you will hear so frequently while learning OOPs
+
+ • class—Simple definition is we can group similar variables & functions together.
+ Ex: Say You are making a computer game, All the functions dealing with enemies  and all the variables that you want in enemy section : That is simply called enemy class
+        Lets say all the charactors are moving all around will have again functions and variables is called moving class. So on & So forth
+        
+        
+ • object— Every object is an instance of a class. Which just means object is part of the class.
+ 
+ • instance—What you get when you tell Python to create a class.
+ 
+ • def—As we already know. def is defining user defined functions/methods. 
+ 
+ • self—Inside the functions in a class, self is a variable for the instance/object being accessed. Its a newthing. You will understand as you grow by.
+ 
+ • inheritance—The concept that one class can inherit traits from another class, much like. you and your parents. Just like Your fathers property is inherited to you. As simple as that. If your father has 100 Rupees. That belongs to you also.
+
+ • composition—The concept that a class can be composed of other classes as parts, much like how a car has wheels.
+
+ • attribute—A property classes have that are from composition and are usually variables. Simple terms is variables.
+ 
+ • is- a—A phrase to say that something inherits from another, as in a “salmon” is- a “fish.”. Just a Narendra is a man. But evey man is not Narendra.
+ 
+ • has- a—A phrase to say that something is composed of other things or has a trait, as in “a salmon has- a mouth.”. Narendra has two hands. But Narendra is not just two hands.
+
+Take some time to make flash cards for those and memorize them. As usual this won’t make too
+much sense until after you’re done with this exercise, but you need to know the base words first.
